2a1167541f735724ad22ed199fbebdd1dc55ea68
DarwinLozada <darwinlozadadev@gmail.com>
2022-10-16 15:31:13 -0400

    add bg image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind(), image()],
+  vite: {
+    ssr: {
+      external: ['@11ty/eleventy-img']
+    }
+  }
+})
