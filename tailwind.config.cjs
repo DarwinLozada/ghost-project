2bfd47990bb04d2e901968ee6412fee567234e59
DarwinLozada <darwinlozadadev@gmail.com>
2022-11-10 18:18:27 -0400

    add new images and add pentagram to page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        cinzel: ['Cinzel']
+      },
+      boxShadow: {
+        album: '0px 3px 20px 8px rgba(0, 0, 0, 0.5)'
+      }
+    }
+  },
+  plugins: []
 }
